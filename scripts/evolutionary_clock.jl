9d32d2485774ab3c65e2342354d1e1fe23e3db67
michielstock <michielfmstock@gmail.com>
2021-01-22 09:03:48 +0100

    evelutionary clock

---
+++ scripts/evolutionary_clock.jl
@@ -0,0 +1,39 @@
+#=
+Created on 13/01/2021 10:16:24
+Last update: -
+
+@author: Michiel Stock
+michielfmstock@gmail.com
+
+Illustration of inference in a simple evolutionary
+clock model.
+=#
+
+using DrWatson
+@quickactivate "PPdemo"
+using Turing, Plots, StatsPlots, StatsBase
+
+
+@model function evolutionary_clock(xs, ts, xn=nothing)
+    #logα ~ Uniform(-2, 4)
+    α ~ LogNormal(0,8)
+    for (i, t) in enumerate(ts)
+        xs[i] ~ Poisson(α * t)
+    end
+    if !isnothing(xn)
+        tn ~ Uniform(0, 10)
+        xn ~ Poisson(α * tn)
+    end
+end
+
+xs = [12, 19, 23, 67, 81]
+ts = [1.0, 2.0, 3.0, 6.0, 7.9]
+
+chain1 = sample(evolutionary_clock(xs, ts), NUTS(), 1000, drop_warmup=true)
+
+plot(chain1)
+
+
+chain2 = sample(evolutionary_clock(xs, ts, 43), NUTS(), 1000, drop_warmup=true)
+
+plot(chain2)
